92a838c4a380f5ea00bd10d2e94e3a2a626d4821
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-16 11:30:59 +0900

    [Add] Code

--- SRS/SRS/ViewController.swift
+++ SRS/SRS/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet weak var mainWebView: WKWebView!
+    
+    let mainWebViewUrl = "http://dsm-rsr.s3-website.ap-northeast-2.amazonaws.com"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        request()
     }
 
 
+}
+
+extension ViewController {
+    
+    func request() {
+        
+        self.mainWebView.load(URLRequest(url: URL(string: mainWebViewUrl)!))
+        self.mainWebView.navigationDelegate = self
+        
+    }
 }
 
