5277fbff943c7f371cd3307b02c53f5a1a1fc2bc
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-16 12:10:35 +0900

    [Add] NetworkDisconnected Function

--- SRS/SRS/NetworkDisConnectedViewController.swift
+++ SRS/SRS/NetworkDisConnectedViewController.swift
@@ -5,4 +5,61 @@
 //  Created by DohyunKim on 2021/03/16.
 //
 
-import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+import SystemConfiguration
+
+class NetworkDisConnectedViewController: UIViewController {
+    
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
+    }
+    
+}
+
+extension NetworkDisConnectedViewController {
+    
+    func isInternetAvailable() -> Bool
+        {
+            var zeroAddress = sockaddr_in()
+            zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+            zeroAddress.sin_family = sa_family_t(AF_INET)
+            
+            let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                    SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+                }
+            }
+            
+            var flags = SCNetworkReachabilityFlags()
+
+            if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+
+                return false
+
+            }
+
+            let isReachable = flags.contains(.reachable)
+            let needsConnection = flags.contains(.connectionRequired)
+
+            return (isReachable && !needsConnection)
+        }
+
+    
+    func bind() {
+        refreshButton.rx.tap
+            .bind {
+                if (self.isInternetAvailable() == true) {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    //toaster 추가 해줄 예정.
+                }
+            }.disposed(by: disposeBag)
+    }
+}
--- SRS/SRS/ViewController.swift
+++ SRS/SRS/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import WebKit
+import SystemConfiguration
 
 class ViewController: UIViewController, WKNavigationDelegate {
 
@@ -17,6 +18,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        networkVaild()
         request()
     }
 
@@ -24,6 +26,45 @@ class ViewController: UIViewController, WKNavigationDelegate {
 }
 
 extension ViewController {
+    
+    func isInternetAvailable() -> Bool
+        {
+            var zeroAddress = sockaddr_in()
+            zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+            zeroAddress.sin_family = sa_family_t(AF_INET)
+            
+            let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                    SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+                }
+            }
+            
+            var flags = SCNetworkReachabilityFlags()
+
+            if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+
+                return false
+
+            }
+
+            let isReachable = flags.contains(.reachable)
+            let needsConnection = flags.contains(.connectionRequired)
+
+            return (isReachable && !needsConnection)
+        }
+
+    
+    func networkVaild() {
+        
+        if(isInternetAvailable() == true) {
+            
+        } else {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "NetworkDisConnectedViewController")
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
     
     func request() {
         
