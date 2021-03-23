562ab338a2a22cb558c46c51b5b75a1e0b5e7bdc
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-23 13:52:22 +0900

    [Add] Native App Project

---
+++ SRS_Native/SRS_Native/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SRS_Native
+//
+//  Created by DohyunKim on 2021/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
