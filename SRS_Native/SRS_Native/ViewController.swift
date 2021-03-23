1237f0c82e8545e92377532bc1638e9cbaf2d81b
DohyunKIm <rlaehgus1229@gmail.com>
2021-03-23 15:44:59 +0900

    [Add] MainViewController

--- SRS_Native/SRS_Native/ViewController.swift
+++ SRS_Native/SRS_Native/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import WebKit
+import SystemConfiguration
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        networkVaild()
     }
-
-
+    
+    
 }
 
+extension ViewController {
+    
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            
+            return false
+            
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
+    }
+    
+    
+    func networkVaild() {
+        
+        if(isInternetAvailable() == true) {
+            print("네트워크 연결 성공")
+        } else {
+            print("네트워크 연결 실패")
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "NetworkDisConnectedViewController")
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+    
+}
